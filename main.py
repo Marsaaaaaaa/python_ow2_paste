53be6e6d9b6928359c5bd369379917bbd51621a7
Marsaaaaaaa <115399891+Marsaaaaaaa@users.noreply.github.com>
2022-10-16 00:44:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ import win32gui
 import win32con
 import os
 import cv2
+ 
 
 # fov, over 290 will break shit, 
-# you ll also need to change this inside grabber.py cause i dont know how python works
 fov = 150
+# lower to get more fps but worse performance
+fps = 144
 
 # list of virtual keys: https://learn.microsoft.com/windows/win32/inputdev/virtual-key-codes
 # default = mouse1
@@ -22,29 +24,25 @@ flick_key = 0x12
 # default = v
 trigger_key = 0x56
 
-# trigger settings, bigger hitbox = shoots more and less accurately
-trigger_sleep = 0.3
-trigger_hitbox = 0.5
+hitbox_size = 0.7
+flick_hitbox_scale = 1
+trigger_sleep = 0
 
-# flick settings, bigger hitbox = shoots more and less accurately
-flick_sleep = 0.3
-flick_hitbox = 1.2
+
+grabber = Grabber(
+    # x and y accel. higher = faster
+    x_multiplier = 0.2,
+    y_multiplier = 0.08,
+    # idk this shit is supposed to set the height of where u aim but I think its broken
+    y_difference = 5,
+    # flick speed multiplier ( flick speed = x_multiplier*flick_speed)
+    flick_speed = 5.5
+)
 
 #closes the cheat
 kill_switch = 0x2E
 
 
-grabber = Grabber(
-    # x and y accel. higher = faster
-    x_multiplier = 0.20,
-    y_multiplier = 0.08,
-    # idk this shit is supposed to set the height of where u aim but I think its broken
-    y_difference = 5,
-    # flick speed multiplier ( flick speed = x_multiplier*flick_speed)
-    flick_speed = 5.2
-)
-
-
 
 #################################################################################################
 #################################################################################################
@@ -52,13 +50,13 @@ grabber = Grabber(
 #################################################################################################
 
 
+left, top = (1920 - fov) // 2, (1080 - fov) // 2
+right, bottom = left + fov, top + fov
+region = (left, top, right, bottom)
+camera = dxcam.create(region=region, output_color="RGB")
+camera.start(target_fps=fps, video_mode=True)
 
 
-
-
-
-
-#camera.start()
 grabber.find_dimensions(fov)
 random_title = grabber.build_title(20)
 system('title ' + f"'{random_title}'")
@@ -74,32 +72,43 @@ print(f"flick speed = {grabber.flick_speed}")
 Dababy = True
 PID = os.getpid()
  
+
 while True:
+
+    if grabber.is_activated(kill_switch):
+        os.kill(PID)               
+
     last_time = time.time()
     count = 0
-    og = grabber.capture_frame()
+    og = np.array(camera.get_latest_frame())
     frame = grabber.process_frame(og)
-    contours = grabber.detect_contours(frame, 1400)
+    contours = grabber.detect_contours(frame, 100)
     if contours:
         rec, x, y = grabber.compute_centroid(contours)
         if grabber.is_activated(aim_key):
             grabber.move_mouse(x, y)
-        else :
-            if grabber.is_activated(flick_key):
-                grabber.flick_mouse(x, y)
-                #grabber.flick_mouse(x/3, y/3)                
-                grabber.flick_trigger(x, flick_sleep, flick_hitbox)
-            else :
-                 if grabber.is_activated(trigger_key):
-                     grabber.trigger(x, trigger_sleep, trigger_hitbox)  
 
-    if grabber.is_activated(kill_switch):
-        os.kill(PID)
+        if grabber.is_activated(flick_key):
+                grabber.flick_mouse(x , y)            
+                if  grabber.on_target(contours, hitbox_size*flick_hitbox_scale):
+                    grabber.trigger()
+
+
+
+
+        if grabber.is_activated(trigger_key) and grabber.on_target(contours, hitbox_size):
+                     time.sleep(trigger_sleep)
+                     grabber.trigger()
+
+    
+
 
 
 
 # uncomment these lines to show conturs and the screen.
 # note: this has a performance impact in my testing.
+
+
         cv2.drawContours(og, contours, -1, (0, 0, 0), 4)
         if rec:
             cv2.drawContours(og, rec, -1, (0, 255, 0), 4)
@@ -107,4 +116,7 @@ while True:
     if (cv2.waitKey(1) & 0x2D) == ord('q'):
         cv2.destroyAllWindows()
         exit()
+
+
+
 
