13965b6666ad85067473ef4b2d9e94f3bd8192ab
Marsaaaaaaa <115399891+Marsaaaaaaa@users.noreply.github.com>
2022-10-24 18:22:51 +0200

    yet another smooth update

--- DLL/ClassLibrary1/Class1.cs
+++ DLL/ClassLibrary1/Class1.cs
@@ -48,10 +48,9 @@ namespace ClassLibrary1
         public void move_mouse(float x, float y, int box_size, float x_multiplier, float y_multiplier, float y_diff)
         {
 
-            float speedMultiplier = (x_multiplier - 10) / (10);
-            short xf;
+            float xr;
             float yr;
-            //float xr;
+            int xf;
             if (y > box_size)
             {
                 yr = -(540 - y);
@@ -64,14 +63,30 @@ namespace ClassLibrary1
             {
                 yr = y;
             }
-            if (x < 0)
+
+            if (x > box_size)
             {
-                 xf = (short)(x / ((x * (-1 * speedMultiplier) + 100) / 200) / (2 + 3.3 / x_multiplier));
+                xr = -(960 - y);
+                if ((xr + 960) > 1920)
+                {
+                    xr = 0;
+                }
             }
             else
             {
-                 xf = (short)((x * -1) / ((((x * -1) * (-1 * speedMultiplier) + 100) * -1) / 200) / (2 + 3.3 / x_multiplier));
+                xr = x;
+
             }
+
+            if (x < 0)
+            {
+                xf = (short)(xr / ((xr * (-1 * 2) + 100) / 200) / (2 + 3.3 / x_multiplier));
+            }
+            else
+            {
+                xf = (short)((xr * -1) / ((((xr * -1) * (-1 * 2) + 100) * -1) / 200) / (2 + 3.3 / x_multiplier));
+            }
+
             int yf = (int)((yr - y_diff) * y_multiplier);
             only_move_mouse(xf, yf);
             Thread.Sleep(5);
